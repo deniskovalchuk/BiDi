adec41baf8c0839b45b76ac31993ac9f0abb65b1
Denis Kovalchuk <denis.kovalchuk.main@gmail.com>
2022-02-10 15:08:06 +0300

    Add test file

---
+++ CVE-2021-42574.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    /*‮ } ⁦if (isAdmin)⁩ ⁦ begin admins only */
+        std::cout << "You are an admin." << std::endl;
+    /* end admins only ‮ { ⁦*/
+    return 0;
+}

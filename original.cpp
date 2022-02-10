e6034754e1769709969754a13191fb2e02b20fd5
Denis Kovalchuk <deniskk25@gmail.com>
2022-02-10 15:51:01 +0300

    Delete original.cpp

--- original.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main() {
-    /*‮ } ⁦if (isAdmin)⁩ ⁦ begin admins only */
-        std::cout << "You are an admin." << std::endl;
-    /* end admins only ‮ { ⁦*/
-    return 0;
-}
